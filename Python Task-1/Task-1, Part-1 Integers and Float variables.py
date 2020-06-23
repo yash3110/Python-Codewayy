a7eeaf209eececfd09a0f67edb901658daa9f528
Yash Pasrija <yashpasrija78407@gmail.com>
2020-06-23 18:22:25 +0530

    added task - 1

---
+++ Python Task-1/Task-1, Part-1 Integers and Float variables.py
@@ -0,0 +1,28 @@
+#Task-1
+#Part 1
+#Sum of two numbers
+
+#Initialize the numbers
+no.1 = 17
+no.2 = 17.64
+
+#Initialize the variables
+sum = 0
+difference = 0
+quotient = 0
+remainder = 0
+product = 0
+
+#Finding the result
+sum = no.1 + no.2
+difference = no.2 - no.1
+quotient = no.2 / no.1
+remainder = no.2 % no.1
+product = no.1 * no.2
+
+#Printing the result
+print("The sum is:", sum)
+print("The difference is:", difference)
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
+print("The product is:", product)
---
+++ Python Task-1/Task-1, Part-2 Student Details.py
@@ -0,0 +1,37 @@
+#Task-1
+#Part 2
+
+#Inputs from the user
+
+#name
+firstName = 'Yash'
+lastName = 'Pasrija'
+
+#college details
+nameoftheCollege = "Amity University"
+addressoftheCollege = "Sec-125, Noida, Uttar Pradesh"
+
+#String Concatenation for name
+name = FirstName + " " + LastName
+
+#String Concatenation for college
+college = NameoftheCollege + " , " + AddressoftheCollege
+
+print("Name of the student: ", Name)
+print("College Details: ", college)
+
+#marks of 5 subjects
+chemistry = 87
+maths = 85
+physics = 83
+english = 97
+painting = 100
+
+#Calculation of the total marks and percentage
+Total-Marks = Chemistry + Maths + Physics + English + Painting
+Percentage = (Total-Marks / 5)
+
+#Print all the data
+
+print("The total marks obtained: ", Total-Marks)
+print("Percentage: ", Percentage)
