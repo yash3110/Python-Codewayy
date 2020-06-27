4e5a94ecae88d8f8e1045e196d73c355a249f830
yash3110 <66896058+yash3110@users.noreply.github.com>
2020-06-27 10:47:51 +0530

    Update main.py

--- Python Task-4/Question-2/main.py
+++ Python Task-4/Question-2/main.py
@@ -2,14 +2,41 @@ import lists
 import operators
 import strings
 
+#Using lists functions
 lists.squareFunc(z)
-lists.minAndMaxFunc(createList)
-lists.sumFunc(createList)
-   
 
+#Taking input from the user
+#Initializing the list
+createList = []
+
+#Taking input for the list
+l = int(input("\nEnter the number of elements you want to include in the list: "))
+print("\nEnter the elements: ")
+for nums in range(l):
+    elements=int(input())
+    createList.append(elements)
+print("Your list is: ", createList)
+
+lists.minAndMaxFunc(createList)
+print("\nThe minimum element is: ", minNum)
+print("The maximum element is:", maxNum)
+
+lists.sumFunc(createList)
+print("The sum of all elements of the list is: ", sum)   
+
+#Using string functions
+#Taking input from the user
+str = input("\nEnter the Word: ")
 strings.stringFunc()
 
+print("Number of vowels are:", vowels)
+print("The number of letters are: ", letters)
 
+#Using Operator functions
+#Taking input from the user
+a=int(input("\nEnter the first number: "))
+b=int(input("Enter the second number: "))
+c=int(input("Enter the third number: "))
 operators.andOper(a,b,c)
 operators.orOper(a,b,c)
 operators.notOper(a)
