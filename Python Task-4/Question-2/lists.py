52ee59f2c45a14a948400f3dd41eb39fb272c53d
yash3110 <66896058+yash3110@users.noreply.github.com>
2020-06-27 10:39:40 +0530

    Update lists.py

--- Python Task-4/Question-2/lists.py
+++ Python Task-4/Question-2/lists.py
@@ -7,17 +7,6 @@ def squarefunc():
     sq = z*z
     print("\nThe square of the number is: ", sq)
 
-#Initializing the list
-createList = []
-
-#Taking input for the list
-l = int(input("\nEnter the number of elements you want to include in the list: "))
-print("\nEnter the elements: ")
-for nums in range(l):
-    elements=int(input())
-    createList.append(elements)
-print("Your list is: ", createList)
-
 #Function for min and max number in a list
 
 def minAndMaxFunc():
@@ -26,21 +15,16 @@ def minAndMaxFunc():
     for i in createList:
         if(minNum > i):
             minNum = i
-    print("\nThe minimum element is: ", minNum)
-
+    
 #For maximum
     maxNum = createList[0] 
     for i in createList:
         if(maxNum < i):
             maxNum = i
-    print("The maximum element is:", maxNum)
     
 #Function for finding the sum of all elements in the list
 def sumfunc():
     sum = 0
     for x in range(0, l):
         sum = sum + createList[x]
-    print("The sum of all elements of the list is: ", sum)
-    
-    
 
